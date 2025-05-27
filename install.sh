fd86d5454db686f709ba24c38930ac031d944b23
EddieVilla <EddieVilla@users.noreply.github.com>
2025-05-27 10:57:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+
+#tmux plugins (resurrect is the first)
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
