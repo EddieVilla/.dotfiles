5e8f6c1cbf15a7a18d57e00954e9eae8babcf997
Eddie Villa <eddie20villa@gmail.com>
2025-05-30 14:33:54 -0500

    added ssh_config

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/.dotfiles/ssh_config $HOME/.ssh/config
 
 #tmux plugins (resurrect is the first)
 mkdir -p ~/.tmux/plugins
